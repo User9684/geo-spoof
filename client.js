71693dc178993f77e0690ba743c2681e1d66ac79
User9684 <user9684official@gmail.com>
2025-03-31 18:47:56 -0500

    Spoof tostring with proxy. Fixes #1

--- client.js
+++ client.js
@@ -37,34 +37,94 @@
         return position;
     }
 
-    window.navigator.geolocation.getCurrentPosition = function (success, fail) {
-        const newEvent = new CustomEvent("gs-request-cpos");
-        events[newEvent] = success;
+    function generateSpoofedFunction(originalFunction, newFunction) {
+        const originalToString =
+            originalFunction.toString.bind(originalFunction);
 
-        document.documentElement.dispatchEvent(newEvent);
-    };
+        function spoofedFunction(...args) {
+            return newFunction(...args);
+        }
+
+        Object.setPrototypeOf(
+            spoofedFunction,
+            Object.getPrototypeOf(originalFunction)
+        );
+
+        // Fake native toString
+        function spoofedToString() {
+            return "function toString() { [native code] }";
+        }
+
+        // Creates a new proxy for infinite toString replacement
+        function createRecursiveToString() {
+            return new Proxy(spoofedToString, {
+                apply() {
+                    return "function toString() { [native code] }";
+                },
+                get(target, prop) {
+                    if (prop === "toString") {
+                        return createRecursiveToString();
+                    }
+                    return Reflect.get(target, prop);
+                },
+            });
+        }
+
+        const infiniteToString = createRecursiveToString();
+
+        // Spoof root toString
+        Object.defineProperty(spoofedFunction, "toString", {
+            value: originalToString,
+            writable: true,
+        });
+
+        // Define spoofed native toString
+        Object.defineProperty(spoofedFunction.toString, "toString", {
+            value: infiniteToString,
+            writable: true,
+            configurable: false,
+        });
+
+        return spoofedFunction;
+    }
+
+    window.navigator.geolocation.getCurrentPosition = generateSpoofedFunction(
+        window.navigator.geolocation.getCurrentPosition,
+        function (success, fail) {
+            const newEvent = new CustomEvent("gs-request-cpos");
+            events[newEvent] = success;
+
+            document.documentElement.dispatchEvent(newEvent);
+        }
+    );
+
+    window.navigator.geolocation.watchPosition = generateSpoofedFunction(
+        window.navigator.geolocation.watchPosition,
+        function (success, fail) {
+            const watcherId = ++watcherIdCounter;
+
+            const newEvent = new CustomEvent("gs-request-watchpos");
+            watchers.set(watcherId, success);
+
+            document.documentElement.dispatchEvent(newEvent);
+
+            return watcherId;
+        }
+    );
+
+    window.navigator.geolocation.clearWatch = generateSpoofedFunction(
+        window.navigator.geolocation.clearWatch,
+        function (watcherId) {
+            if (watchers.has(watcherId)) {
+                watchers.delete(watcherId);
+            }
+        }
+    );
 
     document.documentElement.addEventListener("gs-response-cpos", async (e) => {
         const generatedPosition = await generateFakePosition(e.detail);
         events[e](generatedPosition);
     });
-
-    window.navigator.geolocation.watchPosition = function (success, fail) {
-        const watcherId = ++watcherIdCounter;
-
-        const newEvent = new CustomEvent("gs-request-watchpos");
-        watchers.set(watcherId, success);
-
-        document.documentElement.dispatchEvent(newEvent);
-
-        return watcherId;
-    };
-
-    window.navigator.geolocation.clearWatch = function (watcherId) {
-        if (watchers.has(watcherId)) {
-            watchers.delete(watcherId);
-        }
-    };
 
     document.documentElement.addEventListener(
         "gs-response-watchpos",
